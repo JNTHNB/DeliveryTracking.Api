0aa0abbca957aa701fcff0680a36f9d90a5562aa
Jon <49577100+JNTHNB@users.noreply.github.com>
2026-02-15 12:25:16 +1300

    Add unit tests for `GetActiveDeliveriesHandler` focusing on repository interaction and query handling

---
+++ test/DeliveryTracking.Application.Tests.Unit/Deliveries/Queries/GetActiveDeliveriesHandlerTests.cs
@@ -0,0 +1,58 @@
+﻿using DeliveryTracking.Application.Interfaces;
+using DeliveryTracking.Application.Queries;
+using DeliveryTracking.Domain.Aggregates;
+
+namespace DeliveryTracking.Application.Tests.Unit.Deliveries.Queries;
+
+public class GetActiveDeliveriesHandlerTests
+{
+    private readonly Mock<IDeliveryRepository> _deliveryRepoMock = new();
+    private readonly GetActiveDeliveriesHandler _handler;
+
+    public GetActiveDeliveriesHandlerTests()
+    {
+        _handler = new GetActiveDeliveriesHandler(_deliveryRepoMock.Object);
+    }
+
+    [Fact]
+    public async Task Handle_ShouldReturnActiveDeliveriesFromRepository()
+    {
+        // Arrange
+        var deliveries = new List<Delivery>
+        {
+            new(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid()),
+            new(Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid())
+        };
+
+        _deliveryRepoMock.Setup(r => r.GetActiveDeliveries())
+            .ReturnsAsync(deliveries);
+
+        var query = new GetActiveDeliveriesQuery();
+
+        // Act
+        var result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Should().BeEquivalentTo(deliveries);
+        _deliveryRepoMock.Verify(r => r.GetActiveDeliveries(), Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_WhenNoActiveDeliveries_ShouldReturnEmptyList()
+    {
+        // Arrange
+        var deliveries = new List<Delivery>();
+
+        _deliveryRepoMock.Setup(r => r.GetActiveDeliveries())
+            .ReturnsAsync(deliveries);
+
+        var query = new GetActiveDeliveriesQuery();
+
+        // Act
+        var result = await _handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        result.Should().BeEmpty();
+        _deliveryRepoMock.Verify(r => r.GetActiveDeliveries(), Times.Once);
+    }
+}
